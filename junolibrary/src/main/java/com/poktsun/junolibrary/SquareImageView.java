e5234aec1cec4d6360a815bb0be767c7cf1f3b8f
Kelvin Sze <icasee@gmail.com>
2017-09-12 16:52:02 +0800

    init

---
+++ junolibrary/src/main/java/com/poktsun/junolibrary/SquareImageView.java
@@ -0,0 +1,33 @@
+package com.poktsun.junolibrary;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+/**
+ * Created by kelvin on 12/9/2017.
+ */
+
+public class SquareImageView extends AppCompatImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
